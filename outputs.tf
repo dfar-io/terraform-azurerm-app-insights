66ddf652254c28d2c2f5c75dcd9a592fe4b9cce5
Dave Farinelli <d@dfar.io>
2019-08-21 13:54:07 -0400

    Added ID to outputs.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instrumentation_key" {
   description = "Instrumentation key provided by resource."
   value       = "${azurerm_application_insights.ai.instrumentation_key}"
 }
+
+output "id" {
+  description = "Object ID of the App Insights instance."
+  value       = "${azurerm_application_insights.ai.id}"
+}
