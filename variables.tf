2aa81a1fef397b7cf860233c0fe9f680f14661d7
Dave Farinelli <d@dfar.io>
2020-01-20 12:19:03 -0500

    Added availability alert

--- variables.tf
+++ variables.tf
@@ -7,8 +7,11 @@ variable "location" {
 variable "rg_name" {
   description = "Name of containing Resource Group."
 }
-
 variable "web_tests" {
   description = "A map of web tests (name and URL) to include with resource."
   default     = {}
+}
+variable "action_group_id" {
+  description = "The ID of the action group to activate for availability alerts."
+  default     = null
 }
