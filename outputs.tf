157ccd1cd5fcc1b2cff29fdee82112cf5e5d31b7
Dave Farinelli <d@dfar.io>
2020-10-29 14:31:35 -0400

    Added connection string output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "instrumentation_key" {
   description = "Instrumentation key provided by resource."
-  value       = "${azurerm_application_insights.ai.instrumentation_key}"
+  value       = azurerm_application_insights.ai.instrumentation_key
+}
+
+output "instrumentation_key" {
+  description = "Connection string provided by resource."
+  value       = azurerm_application_insights.ai.connection_string
 }
 
 output "id" {
   description = "Object ID of the App Insights instance."
-  value       = "${azurerm_application_insights.ai.id}"
+  value       = azurerm_application_insights.ai.id
 }
